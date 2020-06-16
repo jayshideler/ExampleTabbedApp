0bb230b1f6f2250f5faa6d085d647d9112309e24
jayshideler <jayshideler@yahoo.com>
2020-06-16 13:24:07 -0600

    Initial Commit

---
+++ ExampleTabbedApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ExampleTabbedApp
+//
+//  Created by Jay Randall Shideler on 6/16/20.
+//  Copyright © 2020 Jay Randall Shideler. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ExampleTabbedApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ExampleTabbedApp
+//
+//  Created by Jay Randall Shideler on 6/16/20.
+//  Copyright © 2020 Jay Randall Shideler. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
