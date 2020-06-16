e5b0a646042094ad21e58d076f855461282aa3d6
jayshideler <jayshideler@yahoo.com>
2020-06-16 15:32:34 -0600

    Create Remote failed to upload files to GitHub

---
+++ ExampleTabbedApp/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  ExampleTabbedApp
+//
+//  Created by Jay Randall Shideler on 6/16/20.
+//  Copyright © 2020 Jay Randall Shideler. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
